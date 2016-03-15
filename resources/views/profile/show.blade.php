207227dd3860589618fa8abc7276212815767652
Arthur Leroux <Arthur@MacBook-Pro-de-Arthur-2.local>
2016-03-15 17:59:38 +0100

    ajout posts sur profil

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -5,10 +5,10 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 @include('errors.message')
-
+                @include('profile.showPosts')
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Modifier mon profil
+                        <h4>Mes informations</h4>
                     </div>
                     <div class="panel-body">
 
--- resources/views/profile/showPosts.blade.php
+++ resources/views/profile/showPosts.blade.php
@@ -1,27 +1,14 @@
-@extends('layouts.app')
+@foreach(Auth::user()->posts as $post)
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <a href="{{ route('post.show', $post->id) }}">
+                <h4>{{ $post->title }}</h4>
+            </a>
+        </div>
 
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                @include('errors.message')
-            </div>
-            @foreach($list as $post)
-                <div class="col-md-10 col-md-offset-1">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <a href="{{ route('post.show', $post->id) }}">
-                                <h4>{{ $post->title }}</h4>
-                            </a>
-                        </div>
-
-                        <div class="panel-body">
-                            <p>{{ $post->content }}</p>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+        <div class="panel-body">
+            <p>{{ $post->content }}</p>
         </div>
     </div>
-@endsection
+@endforeach
 
